4adcccdc568eefc45bbdad75bacf0504f1d67633
root <root@localhost.localdomain>
2018-06-23 12:51:15 +0300

    seed update

--- Jenkins/CI_CD_Seed.groovy
+++ Jenkins/CI_CD_Seed.groovy
@@ -14,3 +14,40 @@ pipelineJob("CI-Job") {
 		}
 	}
 }
+
+pipelineJob("CD_job") {
+	parameters {
+		gitParameterDefinition {
+			name('IMAGE_TAG')
+			branch('master')
+	    	defaultValue('latest')
+	    	listSize('0')
+	    	selectedValue('DEFAULT')
+	    	sortMode('DESCENDING_SMART')
+	    	type('PT_TAG')
+			description('')
+			tagFilter('*')
+			useRepository(gitUrl)
+			quickFilterEnabled(false)
+	    }
+	}
+
+	triggers {
+		upstream('CI_job', 'SUCCESS')
+	}
+
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url("https://github.com/Kontafer/students-project-2018.git")
+						credentials('jenkins-github')
+					}
+					branch('master')
+				}
+			}
+			scriptPath('Jenkins/CD_job.groovy')
+		}
+	}
+}
